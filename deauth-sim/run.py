da98af3031ff51a653c5e4c97b0485780aabd12a
Aida Smith <meep3692@gmail.com>
2026-03-29 02:49:21 -0400

    Message passing

--- deauth-sim/run.py
+++ deauth-sim/run.py
@@ -1,1 +1,10 @@
+import simulation as sim
+import time
 
+if __name__ == "__main__":
+    ether = sim.Ether()
+    ap = sim.AccessPoint("DE:AD:BE:EF:01", {}, ether)
+    ether.send("Hello")
+    time.sleep(4)
+    ether.send("Hello again")
+    ap.join()
--- deauth-sim/simulation.py
+++ deauth-sim/simulation.py
@@ -1,1 +1,32 @@
+import multiprocessing as mp
 
+class Ether:
+    def __init__(self):
+        manager = mp.Manager()
+        self.val = manager.dict()
+        self.write = manager.Lock()
+        self.read = manager.Event()
+    
+    def send(self, message):
+        with self.write:
+            self.val["message"] = message
+            self.read.set()
+        self.read.clear()
+    
+    def recv(self):
+        self.read.wait()
+        return self.val["message"]
+
+class AccessPoint:
+    def __init__(self, mac, protocol, ether):
+        self.mac = mac
+        self.protocol = protocol
+        self.ether = ether
+        self.p = mp.Process(target=self.listen)
+        self.p.start()
+
+    def listen(self):
+        print(self.ether.recv())
+    
+    def join(self):
+        self.p.join()
