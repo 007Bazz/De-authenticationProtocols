efb3323b745e54153f84ef8372a706e144645099
Aida Smith <meep3692@gmail.com>
2026-03-29 23:14:39 -0400

    letter envelope key size configurable

--- deauth-sim/protocols/letter_envelope.py
+++ deauth-sim/protocols/letter_envelope.py
@@ -3,22 +3,22 @@ import sympy
 KEY_SIZE = 512
 
 class LetterEnvelope:
-    def __init__(self):
-        pass
+    def __init__(self, key_size=KEY_SIZE):
+        self.key_size = key_size
 
     def ap_start(self):
         """Generate initial data for AP. Will be passed to assoc_client later as ap_data"""
         #This is the master key to deassoc all clients if the ap goes down
-        p2 = sympy.randprime(2**(KEY_SIZE-1),(2**KEY_SIZE)-1)
-        q2 = sympy.randprime(2**(KEY_SIZE-1),(2**KEY_SIZE)-1)
+        p2 = sympy.randprime(2**(self.key_size-1),(2**self.key_size)-1)
+        q2 = sympy.randprime(2**(self.key_size-1),(2**self.key_size)-1)
         N2 = p2*q2
         return {"envelope": N2, "letter": p2}
 
     def ap_assoc(self, ap_data, body):
         if not "envelope" in body:
             return (False)
-        pi = sympy.randprime(2**(KEY_SIZE-1),(2**KEY_SIZE)-1)
-        qi = sympy.randprime(2**(KEY_SIZE-1),(2**KEY_SIZE)-1)
+        pi = sympy.randprime(2**(self.key_size-1),(2**self.key_size)-1)
+        qi = sympy.randprime(2**(self.key_size-1),(2**self.key_size)-1)
         Ni = pi*qi
         return (True, {"client_envelope": body["envelope"], "ap_letter": pi}, {"envelopes": [Ni, ap_data["envelope"]]})
     
@@ -37,8 +37,8 @@ class LetterEnvelope:
         return {"letter": ap_data["letter"]}
     
     def client_assoc(self):
-        p1 = sympy.randprime(2**(KEY_SIZE-1),(2**KEY_SIZE)-1)
-        q1 = sympy.randprime(2**(KEY_SIZE-1),(2**KEY_SIZE)-1)
+        p1 = sympy.randprime(2**(self.key_size-1),(2**self.key_size)-1)
+        q1 = sympy.randprime(2**(self.key_size-1),(2**self.key_size)-1)
         N1 = p1*q1
         return ({"letter": p1}, {"envelope": N1})
     
